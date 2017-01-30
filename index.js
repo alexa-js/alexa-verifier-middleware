a1acb210be662057d5c9a18a2d4499c51b3de92d
Mike Reinstein <reinstein.mike@gmail.com>
2017-01-30 09:19:30 -0800

    removed semicolons

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var verifier = require('alexa-verifier');
+var verifier = require('alexa-verifier')
+
 
 // the alexa API calls specify an HTTPS certificate that must be validated.
 // the validation uses the request's raw POST body which isn't available from
@@ -8,16 +9,16 @@ var verifier = require('alexa-verifier');
 // middlewares don't try to parse the body again
 module.exports = function alexaVerifierMiddleware(options) {
 	return function(req, res, next) {
-		var strictHeaderCheck = (typeof options !== 'undefined' && typeof options.strictHeaderCheck !== 'undefined' && options.strictHeaderCheck === true);
+		var strictHeaderCheck = (typeof options !== 'undefined' && typeof options.strictHeaderCheck !== 'undefined' && options.strictHeaderCheck === true)
 
 		if (!req.headers.signaturecertchainurl) {
 			// by default, strict header checking isn't enforced
 			if (strictHeaderCheck) {
 				// respond with a 401 error
-				return res.status(401).json({ status: 'failure', reason: 'The signaturecertchainurl HTTP request header is invalid!' });
+				return res.status(401).json({ status: 'failure', reason: 'The signaturecertchainurl HTTP request header is invalid!' })
 			} else {
 				// ignore the check
-				return next();
+				return next()
 			}
 		}
 
@@ -25,42 +26,42 @@ module.exports = function alexaVerifierMiddleware(options) {
 			// by default, strict header checking isn't enforced
 			if (strictHeaderCheck) {
 				// respond with a 401 error
-				return res.status(401).json({ status: 'failure', reason: 'The signature HTTP request header is invalid!' });
+				return res.status(401).json({ status: 'failure', reason: 'The signature HTTP request header is invalid!' })
 			} else {
 				// ignore the check
-				return next();
+				return next()
 			}
 		}
 
 		// mark the request body as already having been parsed so it's ignored by
 		// other body parser middlewares
-		req._body = true;
-		req.rawBody = '';
+		req._body = true
+		req.rawBody = ''
 		req.on('data', function(data) {
-			return req.rawBody += data;
-		});
+			return req.rawBody += data
+		})
 
 		req.on('end', function() {
-			var cert_url, er, error, requestBody, signature;
+			var cert_url, er, error, requestBody, signature
 
 			try {
-				req.body = JSON.parse(req.rawBody);
+				req.body = JSON.parse(req.rawBody)
 			} catch (error) {
-				er = error;
-				req.body = {};
+				er = error
+				req.body = {}
 			}
 
-			cert_url = req.headers.signaturecertchainurl;
-			signature = req.headers.signature;
-			requestBody = req.rawBody;
+			cert_url = req.headers.signaturecertchainurl
+			signature = req.headers.signature
+			requestBody = req.rawBody
 
 			verifier(cert_url, signature, requestBody, function(er) {
 				if (er) {
-					res.status(401).json({ status: 'failure', reason: er });
+					res.status(401).json({ status: 'failure', reason: er })
 				} else {
-					next();
+					next()
 				}
-			});
-		});
-	};
+			})
+		})
+	}
 }
