7105944944b6e5d22bd3327ccceb3e77ca3f170c
Mike Reinstein <reinstein.mike@gmail.com>
2017-09-01 12:01:35 -0700

    minor style cleanup in tests

--- test/index.js
+++ test/index.js
@@ -1,50 +1,51 @@
 'use strict'
 
-var test = require('tap').test;
-var verifier = require('../');
-var httpMocks = require('node-mocks-http');
-var sinon = require('sinon');
+var test      = require('tap').test
+var verifier  = require('../')
+var httpMocks = require('node-mocks-http')
+var sinon     = require('sinon')
 
-var invokeMiddleware = function(data, next, after) {
-  var callbacks = {};
 
-  data['method'] = data['method'] || 'POST';
+function invokeMiddleware(data, next, after) {
+  var callbacks = {}
+
+  data['method'] = data['method'] || 'POST'
   data['on'] = function(eventName, callback) {
-    callbacks[eventName] = callback;
-  };
+    callbacks[eventName] = callback
+  }
 
-  var mockReq = httpMocks.createRequest(data);
-  var mockRes = httpMocks.createResponse();
+  var mockReq = httpMocks.createRequest(data)
+  var mockRes = httpMocks.createResponse()
 
   next = next || function() {}
 
   // verifier is an express middleware (i.e., function(req, res, next) { ... )
-  verifier(mockReq, mockRes, next);
+  verifier(mockReq, mockRes, next)
 
   if (callbacks['data']) {
-    callbacks['data'](data['body']);
+    callbacks['data'](data['body'])
   }
 
   if (callbacks['end']) {
-    callbacks['end']();
+    callbacks['end']()
   }
 
-  process.nextTick(after, mockRes);
-};
+  process.nextTick(after, mockRes)
+}
 
 test('enforce strict headerCheck always', function(t) {
   var nextInvocationCount = 0
   var mockNext = function() { nextInvocationCount++ }
   var mockRes = invokeMiddleware({}, mockNext, function(mockRes) {
-    t.equal(mockRes.statusCode, 401);
+    t.equal(mockRes.statusCode, 401)
     t.deepEqual(JSON.parse(mockRes._getData()), {
       reason: 'missing certificate url',
       status: 'failure'
-    });
+    })
     t.equal(nextInvocationCount, 0)
     t.end()
-  });
-});
+  })
+})
 
 test('fail if request body is already parsed', function(t) {
   var mockRes = invokeMiddleware({
@@ -52,14 +53,14 @@ test('fail if request body is already parsed', function(t) {
     _body: true,
     rawBody: {}
   }, null, function(mockRes) {
-    t.equal(mockRes.statusCode, 400);
+    t.equal(mockRes.statusCode, 400)
     t.deepEqual(JSON.parse(mockRes._getData()), {
       reason: 'The raw request body has already been parsed.',
       status: 'failure'
-    });
+    })
 
-    t.end();
-  });
+    t.end()
+  })
 })
 
 test('fail invalid signaturecertchainurl header', function(t) {
@@ -75,15 +76,15 @@ test('fail invalid signaturecertchainurl header', function(t) {
       }
     }),
   }, null, function(mockRes) {
-    t.equal(mockRes.statusCode, 401);
+    t.equal(mockRes.statusCode, 401)
     t.deepEqual(JSON.parse(mockRes._getData()), {
       reason: 'Certificate URI hostname must be s3.amazonaws.com: invalid',
       status: 'failure'
-    });
+    })
 
     t.end()
-  });
-});
+  })
+})
 
 test('fail invalid JSON body', function(t) {
   var mockRes = invokeMiddleware({
@@ -93,15 +94,15 @@ test('fail invalid JSON body', function(t) {
     },
     body: 'invalid'
   }, null, function(mockRes) {
-    t.equal(mockRes.statusCode, 401);
+    t.equal(mockRes.statusCode, 401)
     t.deepEqual(JSON.parse(mockRes._getData()), {
       reason: 'request body invalid json',
       status: 'failure'
-    });
+    })
 
     t.end()
-  });
-});
+  })
+})
 
 test('fail invalid signature', function(t) {
   var mockRes = invokeMiddleware({
@@ -115,50 +116,50 @@ test('fail invalid signature', function(t) {
       }
     })
   }, function() {
-    calledNext = true;
+    calledNext = true
   }, function(mockRes) {
-    var calledNext = false;
+    var calledNext = false
     setTimeout(function() {
-      t.equal(calledNext, false);
-      t.equal(mockRes.statusCode, 401);
+      t.equal(calledNext, false)
+      t.equal(mockRes.statusCode, 401)
       t.deepEqual(JSON.parse(mockRes._getData()), {
         reason: 'invalid signature',
         status: 'failure'
-      });
-      t.end();
-    }, 2000);
-  });
-});
+      })
+      t.end()
+    }, 2000)
+  })
+})
 
 test('with express.js and body-parser incorrectly mounted', function(t) {
-  var express = require('express');
-  var app = express();
-  app.use(require('body-parser').json());
-  app.use(verifier);
-  var server = app.listen(3000);
-  var request = require('supertest');
+  var express = require('express')
+  var app = express()
+  app.use(require('body-parser').json())
+  app.use(verifier)
+  var server = app.listen(3000)
+  var request = require('supertest')
   request(server)
     .post('/')
     .send({ x: 1 })
     .set('signaturecertchainurl', 'dummy')
     .set('signature', 'aGVsbG8NCg==')
     .end(function (err, res) {
-      t.equal(res.statusCode, 400);
+      t.equal(res.statusCode, 400)
       t.deepEqual(res.body, {
         "reason": "The raw request body has already been parsed.",
         "status": "failure"
-      });
-      server.close();
-      t.end();
-    });
-});
+      })
+      server.close()
+      t.end()
+    })
+})
 
 test('pass with correct signature', function(t) {
-  var timeout = global.setTimeout; // see https://github.com/sinonjs/sinon/issues/269
-  var ts = '2017-02-10T07:27:59Z';
-  var now = new Date(ts);
-  var clock = sinon.useFakeTimers(now.getTime());
-  var calledNext = false;
+  var timeout = global.setTimeout // see https://github.com/sinonjs/sinon/issues/269
+  var ts = '2017-02-10T07:27:59Z'
+  var now = new Date(ts)
+  var clock = sinon.useFakeTimers(now.getTime())
+  var calledNext = false
   var mockRes = invokeMiddleware({
     headers: {
       signature: 'Qc8OuaGEHWeL/39XTEDYFbOCufYWpwi45rqmM2R4WaSEYcSXq+hUko/88wv48+6SPUiEddWSEEINJFAFV5auYZsnBzqCK+SO8mGNOGHmLYpcFuSEHI3eA3nDIEARrXTivqqbH/LCPJHc0tqNYr3yPZRIR2mYFndJOxgDNSOooZX+tp2GafHHsjjShCjmePaLxJiGG1DmrL6fyOJoLrzc0olUxLmnJviS6Q5wBir899TMEZ/zX+aiBTt/khVvwIh+hI/PZsRq/pQw4WAvQz1bcnGNamvMA/TKSJtR0elJP+TgCqbVoYisDgQXkhi8/wonkLhs68pN+TurbR7GyC1vxw==',
@@ -189,13 +190,13 @@ test('pass with correct signature', function(t) {
       }
     })
   }, function() {
-    calledNext = true;
+    calledNext = true
   }, function(mockRes) {
-    t.equal(mockRes.statusCode, 200);
+    t.equal(mockRes.statusCode, 200)
     timeout(function() {
-      t.equal(calledNext, true);
-      clock.restore();
-      t.end();
-    }, 2000);
-  });
-});
+      t.equal(calledNext, true)
+      clock.restore()
+      t.end()
+    }, 2000)
+  })
+})
