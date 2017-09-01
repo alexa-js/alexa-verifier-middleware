a39637af833fb334fc37c2e65fa7dbe524c70c36
Tejas Shah <tejashah88@gmail.com>
2017-09-01 00:28:11 -0700

    updated alexa-verifier to v0.4.0

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var verifier = require('../');
 var httpMocks = require('node-mocks-http');
 var sinon = require('sinon');
 
-var invokeMiddleware = function(data, next) {
+var invokeMiddleware = function(data, next, after) {
   var callbacks = {};
 
   data['method'] = data['method'] || 'POST';
@@ -28,21 +28,22 @@ var invokeMiddleware = function(data, next) {
     callbacks['end']();
   }
 
-  return mockRes;
+  process.nextTick(() => after(mockRes));
+  //return mockRes;
 };
 
 test('enforce strict headerCheck always', function(t) {
   var nextInvocationCount = 0
   var mockNext = function() { nextInvocationCount++ }
-  var mockRes = invokeMiddleware({}, mockNext);
-
-  t.equal(mockRes.statusCode, 401);
-  t.deepEqual(JSON.parse(mockRes._getData()), {
-    reason: 'missing certificate url',
-    status: 'failure'
+  var mockRes = invokeMiddleware({}, mockNext, function(mockRes) {
+    t.equal(mockRes.statusCode, 401);
+    t.deepEqual(JSON.parse(mockRes._getData()), {
+      reason: 'missing certificate url',
+      status: 'failure'
+    });
+    t.equal(nextInvocationCount, 0)
+    t.end()
   });
-  t.equal(nextInvocationCount, 0)
-  t.end()
 });
 
 test('fail if request body is already parsed', function(t) {
@@ -50,15 +51,15 @@ test('fail if request body is already parsed', function(t) {
     headers: {},
     _body: true,
     rawBody: {}
-  });
+  }, null, function(mockRes) {
+    t.equal(mockRes.statusCode, 400);
+    t.deepEqual(JSON.parse(mockRes._getData()), {
+      reason: 'The raw request body has already been parsed.',
+      status: 'failure'
+    });
 
-  t.equal(mockRes.statusCode, 400);
-  t.deepEqual(JSON.parse(mockRes._getData()), {
-    reason: 'The raw request body has already been parsed.',
-    status: 'failure'
+    t.end();
   });
-
-  t.end();
 })
 
 test('fail invalid signaturecertchainurl header', function(t) {
@@ -73,15 +74,15 @@ test('fail invalid signaturecertchainurl header', function(t) {
         timestamp: new Date().getTime()
       }
     }),
-  });
+  }, null, function(mockRes) {
+    t.equal(mockRes.statusCode, 401);
+    t.deepEqual(JSON.parse(mockRes._getData()), {
+      reason: 'Certificate URI hostname must be s3.amazonaws.com: invalid',
+      status: 'failure'
+    });
 
-  t.equal(mockRes.statusCode, 401);
-  t.deepEqual(JSON.parse(mockRes._getData()), {
-    reason: 'Certificate URI hostname must be s3.amazonaws.com: invalid',
-    status: 'failure'
+    t.end()
   });
-
-  t.end()
 });
 
 test('fail invalid JSON body', function(t) {
@@ -91,15 +92,15 @@ test('fail invalid JSON body', function(t) {
       signaturecertchainurl: 'https://invalid'
     },
     body: 'invalid'
-  });
+  }, null, function(mockRes) {
+    t.equal(mockRes.statusCode, 401);
+    t.deepEqual(JSON.parse(mockRes._getData()), {
+      reason: 'request body invalid json',
+      status: 'failure'
+    });
 
-  t.equal(mockRes.statusCode, 401);
-  t.deepEqual(JSON.parse(mockRes._getData()), {
-    reason: 'request body invalid json',
-    status: 'failure'
+    t.end()
   });
-
-  t.end()
 });
 
 test('fail invalid signature', function(t) {
@@ -115,18 +116,18 @@ test('fail invalid signature', function(t) {
     })
   }, function() {
     calledNext = true;
+  }, function(mockRes) {
+    var calledNext = false;
+    setTimeout(function() {
+      t.equal(calledNext, false);
+      t.equal(mockRes.statusCode, 401);
+      t.deepEqual(JSON.parse(mockRes._getData()), {
+        reason: 'invalid signature',
+        status: 'failure'
+      });
+      t.end();
+    }, 2000);
   });
-
-  var calledNext = false;
-  setTimeout(function() {
-    t.equal(calledNext, false);
-    t.equal(mockRes.statusCode, 401);
-    t.deepEqual(JSON.parse(mockRes._getData()), {
-      reason: 'invalid signature',
-      status: 'failure'
-    });
-    t.end();
-  }, 2000);
 });
 
 test('with express.js and body-parser incorrectly mounted', function(t) {
@@ -189,12 +190,12 @@ test('pass with correct signature', function(t) {
     })
   }, function() {
     calledNext = true;
+  }, function(mockRes) {
     t.equal(mockRes.statusCode, 200);
+    timeout(function() {
+      t.equal(calledNext, true);
+      clock.restore();
+      t.end();
+    }, 2000);
   });
-
-  timeout(function() {
-    t.equal(calledNext, true);
-    clock.restore();
-    t.end();
-  }, 2000);
 });
