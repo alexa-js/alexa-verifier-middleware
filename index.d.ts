08e639d14c91413415cb41b222902b41867b2f34
evandrouzeda <49155191+evandrouzeda@users.noreply.github.com>
2022-09-26 09:50:18 -0300

    adding more expecifications

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-export default function alexaVerifierMiddleware (req: any, res: any, next: any): any
+import {Request, Response, NextFunction} from "express"
+/**
+ * This function lookup for requests that include a signaturecertchainurl HTTP in header, parse out the entire body as a text string, and set a flag on the request object so others don't try to parse the body again. 
+ * {@link https://github.com/alexa-js/alexa-verifier-middleware See the Documentation}
+ */
+declare function alexaVerifierMiddleware (req: Request, res: Response, next: NextFunction): NextFunction
+
+export = alexaVerifierMiddleware
